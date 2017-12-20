f29ac62d938a47ca160e80f495104da92f8ed33a
Anastasia Chinsky <anastasia.chinsky@isep.fr>
2017-12-20 12:45:31 +0100

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,12 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp", { useMongoClient: true });
+//local db
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp",
+  { useMongoClient: true }
+);
 
 module.exports = {
   mongoose
 };
-
